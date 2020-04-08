d32f887c37e8e9915f50d982e3a54aa20851c4c2
Seth James Nielson <seth@crimsonvista.com>
2020-04-08 03:02:31 -0500

    Added code for first cut of network classroom

---
+++ 2020sp_cs316s/network_classroom/src/network_classroom/marshall.py
@@ -0,0 +1,20 @@
+import json
+
+"""
+Messages:
+  <header len> <payload len>\n
+  json
+  binary
+"""
+
+def marshall(d, payload=b""):
+    json_str = json.dumps(d)
+    b = "{} {}\n{}".format(len(json_str), len(payload), json_str)
+    return b.encode() + payload
+    
+def unmarshall(data):
+    lens, data = data.split(b"\n", maxsplit=1)
+    hlen, plen = [int(l.strip()) for l in lens.split(b" ")]
+    json_bytes, data = data[:hlen], data[hlen:]
+    payload, r = data[:plen], data[plen:]
+    return json.loads(json_bytes.decode()), payload, r
