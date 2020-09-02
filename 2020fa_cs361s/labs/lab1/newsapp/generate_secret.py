f0d3b44a67abb29e7f3699f607beb73687b77956
Seth James Nielson <seth@crimsonvista.com>
2020-09-02 12:03:27 -0500

    Added attribution

--- 2020fa_cs361s/labs/lab1/newsapp/generate_secret.py
+++ 2020fa_cs361s/labs/lab1/newsapp/generate_secret.py
@@ -1,3 +1,6 @@
+## Create a secret for the local installation (not stored in git)
+## Taken from https://humberto.io/blog/tldr-generate-django-secret-key
+
 from django.core.management.utils import get_random_secret_key
 
 with open("django_secret_key.txt", "w+") as f:
