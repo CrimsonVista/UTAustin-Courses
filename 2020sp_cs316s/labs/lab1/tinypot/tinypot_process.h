257db734b649732afd5dc69926b3bd3254b700e4
ishanphadke <44324805+ishanphadke@users.noreply.github.com>
2020-10-21 21:00:43 -0500

    Fixed directory structure

--- 2020sp_cs316s/labs/lab1/tinypot/tinypot_process.h
+++
@@ -1,7 +0,0 @@
-#ifndef _TINYPOT_PROCESS_H_
-#define _TINYPOT_PROCESS_H_
-
-int process_connection (int con_num, int port_num, int socketFD);
-char* my_time (void);
-
-#endif /* _TINYPOT_PROCESS_H_ */
